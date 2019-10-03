b8a97683232141e989f564fe8fae03e4965732b4
Dastan Tulokulov <dastan.tulokulov.kg@mail.ru>
2019-10-03 17:13:04 +0600

    DONE!

--- app/src/main/java/com/example/dastantulokulov/m3homework2/MainActivity.java
+++ app/src/main/java/com/example/dastantulokulov/m3homework2/MainActivity.java
@@ -3,12 +3,198 @@ package com.example.dastantulokulov.m3homework2;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    String num1, num2, sum;
+
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnPlus, btnMinus, btnP, btnDiv, btnEqual, btnDot, btnC;
+    EditText editText;
+
+    float Value1, Value2;
+    boolean PLUS, MINUS, PERCENT, DIVIDE;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn0 = (Button) findViewById(R.id.num0);
+        btn1 = (Button) findViewById(R.id.num1);
+        btn2 = (Button) findViewById(R.id.num2);
+        btn3 = (Button) findViewById(R.id.num3);
+        btn4 = (Button) findViewById(R.id.num4);
+        btn5 = (Button) findViewById(R.id.num5);
+        btn6 = (Button) findViewById(R.id.num6);
+        btn7 = (Button) findViewById(R.id.num7);
+        btn8 = (Button) findViewById(R.id.num8);
+        btn9 = (Button) findViewById(R.id.num9);
+        btnPlus = (Button) findViewById(R.id.BtnPlus);
+        btnDiv = (Button) findViewById(R.id.BtnDiv);
+        btnMinus = (Button) findViewById(R.id.BtnMinus);
+        btnP = (Button) findViewById(R.id.BtnPhr);
+        btnEqual = (Button) findViewById(R.id.BtnEqual);
+        btnDot = (Button) findViewById(R.id.BtnDot);
+        btnC = (Button) findViewById(R.id.BtnC);
+        editText = (EditText) findViewById(R.id.numField);
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "0");
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "1");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "2");
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "3");
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "4");
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "5");
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "6");
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "7");
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "8");
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "9");
+            }
+        });
+
+        btnDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + ".");
+            }
+        });
+
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (editText == null) {
+                    editText.setText("");
+                } else {
+                    Value1 = Float.parseFloat(editText.getText() + "");
+                    PLUS = true;
+                    editText.setText(null);
+                }
+            }
+        });
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Value1 = Float.parseFloat(editText.getText() + "");
+                MINUS = true;
+                editText.setText(null);
+            }
+        });
+
+        btnP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Value1 = Float.parseFloat(editText.getText() + "");
+                PERCENT = true;
+                editText.setText(null);
+            }
+        });
+
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Value1 = Float.parseFloat(editText.getText() + "");
+                DIVIDE = true;
+                editText.setText(null);
+            }
+        });
+
+        btnEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Value2 = Float.parseFloat(editText.getText() + "");
+
+                if (PLUS == true) {
+
+                    editText.setText(Value1 + Value2 + "");
+                    PLUS = false;
+                }
+
+
+                if (MINUS == true) {
+                    editText.setText(Value1 - Value2 + "");
+                    MINUS = false;
+                }
+
+                if (PERCENT == true) {
+                    editText.setText(Value1 * Value2 / 100 + "");
+                    PERCENT = false;
+                }
+
+                if (DIVIDE == true) {
+                    editText.setText(Value1 / Value2 + "");
+                    DIVIDE = false;
+                }
+            }
+        });
+
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText("");
+            }
+        });
+    }
+    /*String num1, num2, sum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -249,7 +435,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
+    }*/
 
 
 }
